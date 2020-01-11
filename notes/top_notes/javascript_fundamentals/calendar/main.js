40e3d2cc01422f1a98426c2ec405c75183d53ffc
Richard Vu <richard.t.vu@gmail.com>
2020-01-11 12:13:11 -0500

    Move calendar app to own directory and type down template for script from MDN

---
+++ notes/top_notes/javascript_fundamentals/calendar/main.js
@@ -0,0 +1,25 @@
+// Check out https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals for script 
+
+const select = document.querySelector('select');
+const list = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+select.onchange = function() {
+    const choice = select.value; 
+
+    // ADD CONDITIONAL HERE
+
+    createCalendar(days, choice); 
+}
+
+function createCalendar(days, choice) {
+    list.innerHTML = '';
+    h1.textContent = choice; 
+    for (let i = 1; i <= days; i++) {
+        const listItem = document.createElement('li'); 
+        listItem.textContent = i; 
+        list.appendChild(listItem); 
+    }
+}
+
+createCalendar(31, 'January'); 
